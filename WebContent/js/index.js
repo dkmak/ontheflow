ecedb8970e9759c368a048b8012bcdff6e245c8b
Darryl Mak <darrylmak2@gmail.com>
2018-08-23 00:05:31 -0700

    implemented functions recieve code and request tokens

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -4,7 +4,7 @@ function getParameterByName(target) {
     // Encode target parameter name to url encoding
     target = target.replace(/[\[\]]/g, "\\$&");
 
-    // Ues regular expression to find matched parameter value
+    // Uses regular expression to find matched parameter value
     let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
     if (!results) return null;
@@ -15,8 +15,21 @@ function getParameterByName(target) {
 }
 
 
+function handleResult(resultData) {
 
+    
+}
+
+
+//This runs once the .js page is loaded
 let code = getParameterByName('code');
 console.log(code);
 
 //take code, make AJAX call to a servlet that sets the code, creates tokens
+//Makes the HTTP GET request and registers on success callback function handleResult
+jQuery.ajax({
+    dataType: "json",  // Setting return data type
+    method: "GET",// Setting request method
+    url: "api/index?code=" + code, // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleResult(resultData) // Setting callback function to handle data returned successfully by the SingleStarServlet
+});
